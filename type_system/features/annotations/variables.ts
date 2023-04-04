e965d5c860e0777a6eac0e89009086004c887b89
koddaks <koss199710@gmail.com>
2023-04-04 11:43:42 +0300

    variables with ts

--- type_system/features/annotations/variables.ts
+++ type_system/features/annotations/variables.ts
@@ -9,3 +9,15 @@ let now: Date = new Date();
 
 // Array
 let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, false, true];
+
+// Classes
+class Car {}
+let car: Car;
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
